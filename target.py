3a096b921620887cc70ddb828a2defb84b2e2f02
Marcus Berg <marcusb@mabsoft.org>
2020-11-03 10:28:31 +0000

    Update target.py
    Added various US timezones

--- target.py
+++ target.py
@@ -17,7 +17,7 @@ cal = pytz.timezone("America/Los_Angeles")
 alaska = pytz.timezone("America/Anchorage")
 
 firstOpen   = nyc.localize(datetime.datetime(2020,11,3,6,0))
-firstClosed = florida.localize(datetime.datetime(2020,11,3,18,0))
+firstClosed = florida.localize(datetime.datetime(2020,11,3,19,0))
 lastClosed  = alaska.localize(datetime.datetime(2020,11,3,20,0))
 
 def strfdelta(tdelta, fmt):
@@ -30,11 +30,15 @@ def timestr(delta, name):
     return name + strfdelta(delta, format)
 
 while True:
-    timenow = local.localize(datetime.datetime.now())
-    screen.addstr(2, 5, timestr(firstOpen   - timenow, "First Open:   "))
-    screen.addstr(5, 5, timestr(firstClosed - timenow, "First Closed: "))
-    screen.addstr(8, 5, timestr(lastClosed  - timenow, "Last Closed:  "))
-    screen.addstr(10, 0, "")
+    timenow = datetime.datetime.now()
+    screen.addstr(2, 5, timestr(firstOpen   - local.localize(timenow), "First Open:   "))
+    screen.addstr(4, 5, timestr(firstClosed - local.localize(timenow), "First Closed: "))
+    screen.addstr(6, 5, timestr(lastClosed  - local.localize(timenow), "Last Closed:  "))
+    screen.addstr(8, 5,  "Local time: " + local.localize(timenow).strftime("%H:%M"))
+    screen.addstr(10, 5, "EST time:   " + datetime.datetime.now(nyc).strftime("%H:%M"))
+    screen.addstr(12, 5, "PST time:   " + datetime.datetime.now(cal).strftime("%H:%M"))
+    screen.addstr(14, 5, "AKST time:  " + datetime.datetime.now(alaska).strftime("%H:%M"))
+    screen.addstr(16, 0, "")
     screen.refresh()
 
     curses.napms(2000)
